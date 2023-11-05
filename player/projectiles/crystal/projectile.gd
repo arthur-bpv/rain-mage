10e0a7d857b88917171d2fb6313fd22dbd256ca8
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2023-11-05 15:50:22 -0300

    new | projectile

--- player/projectiles/crystal/projectile.gd
+++ player/projectiles/crystal/projectile.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+var SPEED = 10;
 
 func _process(delta):
-	position += transform.x * 200 * delta;
+	position += transform.x * SPEED * delta;
+	SPEED += 10;
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free();
